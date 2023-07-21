f3e81f990b85b34c23030a4b02b30ea981dff7ac
Brian Borra <brio50@gmail.com>
2023-07-20 22:24:58 -0700

    serious refactor of tech stack, working unit conversions, divisor dropdown not working yet nor fraction algorithm

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [millimeters, setMM] = useState(null);
+  const [inches, setIN] = useState(null);
+  const [feet, setFT] = useState(null);
+
+  const onChangeMM = (event) => {
+    setMM(event.target.value);
+    setIN(mm2in(event.target.value));
+    setFT(in2ft(mm2in(event.target.value)));
+  };
+  const onChangeIN = (event) => {
+    setMM(in2mm(event.target.value));
+    setIN(event.target.value);
+    setFT(in2ft(event.target.value));
+  };
+  const onChangeFT = (event) => {
+    setMM(in2mm(ft2in(event.target.value)));
+    setIN(ft2in(event.target.value));
+    setFT(event.target.value);
+  };
+
+  function mm2in(millimeters) { return (millimeters * 0.0393701) }
+  function in2mm(inches) { return (inches / 0.0393701) }
+  function in2ft(inches) { return (inches / 12.0) }
+  function ft2in(feet) { return (feet * 12.0) }
+
+  return (
+
+    <main class="flex-shrink-1" role="main">
+
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+
+      <div className="container">
+
+        <div className="row">
+          <div class="input-group">
+            <select className="form-select align-middle mb-3" aria-label="divisor">
+              <option>Select a divisor:</option>
+              <option value="1">1/64</option>
+              <option value="2">1/32</option>
+              <option value="3">1/16</option>
+              <option value="4">1/8</option>
+              <option value="5">1/4</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="row">
+          <label for="input-mm" class="form-label">Millimeters</label>
+          <div class="input-group">
+            <input id="input-mm" value={millimeters} onChange={onChangeMM} type="number" class="form-control" />
+            <div class="input-group-append">
+              <span class="input-group-text"> mm </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <label for="input-mm" class="form-label">Inches</label>
+          <div class="input-group">
+            <input id="input-mm" value={inches} onChange={onChangeIN} type="number" class="form-control" />
+            <div class="input-group-append">
+              <span class="input-group-text"> in </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <label for="input-mm" class="form-label">Feet + Inches</label>
+          <div class="input-group">
+            <input id="input-mm" value={feet} onChange={onChangeFT} type="number" class="form-control" />
+            <div class="input-group-append">
+              <span class="input-group-text"> ft+in </span>
+            </div>
+          </div>
+        </div>
+
+      </div >
+
+    </main >
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+/* Bootstrap, Fontawesome, Jquery, Popper */
+import "bootstrap/dist/css/bootstrap.css";
+import '@fortawesome/fontawesome-free/js/all.js';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+var rootNode = document.getElementById('root');
+const root = createRoot(rootNode);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
